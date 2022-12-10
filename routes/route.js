644bd432fc9eeb2ecc89bf3d9eec0fe2125d12e0
hritikdubey22 <hritik.gloryautotech@gmail.com>
2022-12-10 17:29:41 +0530

    hjhate

--- routes/route.js
+++ routes/route.js
@@ -3,9 +3,5 @@ const router = express.Router();
 let controller  = require("../controller/controller")
 router.post("/createRecords",controller.createRecords)
 router.post("/createCondition",controller.createCondition)
-<<<<<<< HEAD
-// gti added comment  
-=======
-
->>>>>>> 460c170f8ddfc7025e3a75ca16c81efc36e1e162
+"hbjnkdl;f"
 module.exports = router; 
